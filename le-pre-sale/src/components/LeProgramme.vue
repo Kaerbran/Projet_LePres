118b82443dfb3b2bf6cfbc4c11820d74397dd6bb
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2022-04-20 19:43:49 +0200

    environnement de dev

---
+++ le-pre-sale/src/components/LeProgramme.vue
@@ -0,0 +1,22 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "LeProgramme",
+    data() {
+        return {}
+    },
+	computed: {},
+    methods: {},
+}
+</script>
+
+<style scoped lang="scss">
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0px 0px 0px 0px ; // top right bottom left
+}
+
+</style>
