f5e217531925f99124e0aed1713d35fe66976c92
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2022-04-23 17:29:16 +0200

    Avancement Nav

--- le_pre_sale/src/App.vue
+++ le_pre_sale/src/App.vue
@@ -14,6 +14,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Monoton&family=Share&display=swap');
+
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0px 0px 0px 0px ; // top right bottom left
--- le_pre_sale/src/components/NavSite.vue
+++ le_pre_sale/src/components/NavSite.vue
@@ -15,6 +15,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Monoton&family=Share&display=swap');
+
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0px 0px 0px 0px ; // top right bottom left
@@ -25,17 +27,34 @@ export default {
     justify-content: center;
 
     background-color: rgba(217, 229, 192, 1);
+    padding: 20px 20px 20px 20px ; // top right bottom left
 
     &__date{
         display: inline-block;
-        margin-left: auto;
+        margin: auto 0px auto auto ; // top right bottom left
+
+        font-family:'Monoton',sans-serif;
+        color: rgba(204, 96, 63, 1);
     }
     &__achat{
         margin-left: auto;
 
     }
     &__achat__btt {
-
+        display:inline-block;
+        padding:0.7em 1.4em;
+        margin:0 0.3em 0.3em 0;
+        border-radius:0.15em;
+        box-sizing: border-box;
+        text-decoration:none;
+        font-family:'Monoton',sans-serif;
+        text-transform:uppercase;
+        font-weight:400;
+        color:#FFFFFF;
+        background-color:rgba(204, 96, 63, 1);
+        box-shadow:inset 0 -0.6em 0 -0.35em rgba(0,0,0,0.17);
+        text-align:center;
+        position:relative;
     }
     &__achat__img{
 
