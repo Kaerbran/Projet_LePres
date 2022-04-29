12855a5b55fef04600396a08abc737857f19ed2a
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2022-04-29 23:47:07 +0200

    comit

--- le_pre_sale/src/components/LaProgrammtion.vue
+++ le_pre_sale/src/components/LaProgrammtion.vue
@@ -6,21 +6,21 @@
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/Thibaut-sibella-Groupe.jpg" alt="">
           </picture>
-          <p>Thibaut Sibella and the Graveyard Shift proposent des compositions originales folk pleines de mélodies et d’émotions à travers laquelle ils dépeignent des histoires, des rencontres, des souvenirs. Une guitare, un banjo, une contre basse et trois voix au service d’une musique inspirée par la folk bluegrass/indie et le rock des années 60 qui se veux pleine de bonnes vibrations.</p>
+          <p class="container__p">Thibaut Sibella and the Graveyard Shift proposent des compositions originales folk pleines de mélodies et d’émotions à travers laquelle ils dépeignent des histoires, des rencontres, des souvenirs. Une guitare, un banjo, une contre basse et trois voix au service d’une musique inspirée par la folk bluegrass/indie et le rock des années 60 qui se veux pleine de bonnes vibrations.</p>
       </div>
       <div class="container__div">
           <h3 class="container__h3">Dudes Of Groove Society<br>(New Funk)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/DOGS.png" alt="">
           </picture>
-          <p>Le groupe préfère définir son style en “New Funk – Hip-Hop” et ne s’embête pas pour mélanger l’éléctro et les musiques actuelles avec la funk de James Brown, ce qu’il en ressort c’est une énergie folle et un show carré au possible !</p>
+          <p class="container__p">Le groupe préfère définir son style en “New Funk – Hip-Hop” et ne s’embête pas pour mélanger l’éléctro et les musiques actuelles avec la funk de James Brown, ce qu’il en ressort c’est une énergie folle et un show carré au possible !</p>
       </div>
       <div class="container__div">
           <h3 class="container__h3">Tadeo<br>(GROOVE / Hip Hop)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/Tadeos.jpeg" alt="">
           </picture>
-          <p>Une envolée de mots posés sur un groove de base Hip Hop. Les textes sentent le bon vin et s'appuient sur des sonorités swing ou funk permettant le déhanchement et la danse !</p>
+          <p class="container__p">Une envolée de mots posés sur un groove de base Hip Hop. Les textes sentent le bon vin et s'appuient sur des sonorités swing ou funk permettant le déhanchement et la danse !</p>
       </div>
   </div>
 </template>
@@ -71,13 +71,19 @@ justify-content: space-around;
         padding: 0px 0px 0px 0px ; // top right bottom left
     }
     &__h3{
-
+        font-family:'Monoton',sans-serif; font-size: 1em;
+        font-weight: 400;
+        text-align: center;
     }
     &__picture{        
         &__img{
             width: 100%;
             object-fit: contain;
         }
+    }
+    &__p{
+        font-family:'Share',sans-serif; font-size: 1em;
+        font-weight: 400;
     }
 }
 
