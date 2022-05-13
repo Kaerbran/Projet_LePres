52270859d3606d234dd2f3031255324f68850e73
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2022-05-13 12:41:15 +0200

    Ajout lien pour sponsors

--- le_pre_sale/src/components/LesSponsors.vue
+++ le_pre_sale/src/components/LesSponsors.vue
@@ -4,18 +4,18 @@
     <div>
         <div class="container__div">
             <picture class="container__picture">
-                <img class="container__picture__img" src="../assets/LOGO_CYNNOTEK.png" alt="Logo de l'entreprise Cynnotek">
+                <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_CYNNOTEK.png" alt="Logo de l'entreprise Cynnotek" onclick="location.href='https://www.cynnotek.fr/';" >
             </picture>
         </div>
         <div class="container__div">
             <picture class="container__picture">
-                <img class="container__picture__img container__picture__img--marge" src="../assets/LOGO_CROSSFER.png" alt="Logo de l'entreprise Projets Julien Bois">
+                <img class="container__picture__img container__picture__img--marge container__picture__img--hover" src="../assets/LOGO_CROSSFER.png" alt="Logo de l'entreprise Projets Julien Bois" onclick="location.href='https://www.crossferfrance.fr/';">
             </picture>
         </div>
     </div>
     <div class="container__div">
         <picture class="container__picture">
-            <img class="container__picture__img" src="../assets/LOGO_DOMAINEMURE.jpg" alt="Logo de l'entreprise Projets Julien Bois">
+            <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_DOMAINEMURE.jpg" alt="Logo de l'entreprise Projets Julien Bois" onclick="location.href='https://www.mure.com/';" >
         </picture>
     </div>
     <div class="container__div">
@@ -26,7 +26,7 @@
     <div>
         <div class="container__div">
             <picture class="container__picture">
-                <img class="container__picture__img" src="../assets/LOGO_MAKITA.png" alt="Logo de l'entreprise Makita">
+                <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_MAKITA.png" alt="Logo de l'entreprise Makita" onclick="location.href='https://www.remymuller.fr/';">
             </picture>
         </div>
         <div class="container__div">
@@ -37,12 +37,12 @@
     </div>
     <div class="container__div">
         <picture class="container__picture">
-            <img class="container__picture__img" src="../assets/LOGO_DIAM.jpeg" alt="Logo de l'entreprise Noumatrouff">
+            <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_DIAM.jpeg" alt="Logo de l'entreprise Noumatrouff" onclick="location.href='https://www.diam-bouchon-liege.com/';">
         </picture>
     </div>
     <div class="container__div">
         <picture class="container__picture">
-            <img class="container__picture__img" src="../assets/LOGO_cellierMuhlbach.jpg" alt="Logo de l'entreprise Noumatrouff">
+            <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_cellierMuhlbach.jpg" alt="Logo de l'entreprise Noumatrouff" onclick="location.href='https://www.noumatrouff.fr/';">
         </picture>
     </div>
     <div class="container__div">
@@ -57,12 +57,17 @@
     </div>
     <div class="container__div">
         <picture class="container__picture">
-            <img class="container__picture__img" src="../assets/LOG_MACK_IMPRESSION.png" alt="Logo de la société Mack Impressions">
+            <img class="container__picture__img container__picture__img--hover" src="../assets/LOG_MACK_IMPRESSION.png" alt="Logo de la société Mack Impressions" onclick="location.href='https://www.imprimerie-mack.com/';">
         </picture>
     </div>
     <div class="container__div">
         <picture class="container__picture">
-            <img class="container__picture__img" src="../assets/LOGO_TTC.jpg" alt="Logo de l'entreprise TTC">
+            <img class="container__picture__img container__picture__img--hover" src="../assets/LOGO_TTC.jpg" alt="Logo de l'entreprise TTC" onclick="location.href='https://www.tt-chauffage.com/';">
+        </picture>
+    </div>
+    <div class="container__div">
+        <picture class="container__picture">
+            <img class="container__picture__img" src="../assets/LOGO_Henninger.jpg" alt="Logo de l'entreprise Henninger">
         </picture>
     </div>
   </div>
@@ -141,6 +146,12 @@ background: linear-gradient(190deg, rgba(131,58,180,1) 20%, rgba(253,29,29,1) 54
 
             &--marge{
                 margin-top: 8px;
+            }
+
+            &--hover{
+                &:hover{
+                    cursor: pointer;
+                }
             }
         }
     }
