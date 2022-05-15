efcb5dce407d03a49232acc0efd222f617c0af24
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2022-05-15 22:26:26 +0100

    icones à jours

--- le_pre_sale/src/components/LaProgrammtion.vue
+++ le_pre_sale/src/components/LaProgrammtion.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
       <div class="container__presentation">
-          Prés salés []<br>
+          Prés salés [pʁe sale]<br>
           (traduction de l'alsacien Sàltz Màtt ou Soultzmatt)<br><br>
           Le Festival Prés Salés c'est quoi?<br><br>
           C'est l'alchemie de sept personnalités habitées par les mêmes passions, guidées par les mêmes convictions. C'est 
@@ -17,7 +17,7 @@
           <h3 class="container__h3">Thibaut Sibella <br>(WildPoP / Funk)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/Thibaut-sibella-Groupe.jpg" alt="Thibaut Sibella et son groupe">
-              <img class="container__picture__youtube" src="../assets/social/facebook.png" onclick="location.href='https://www.youtube.com/watch?v=7NjXZ0IffcQ';" />
+              <img class="container__picture__youtube" src="../assets/social/youtube.png" onclick="location.href='https://www.youtube.com/watch?v=7NjXZ0IffcQ';" />
           </picture>
           <p class="container__p">Thibaut Sibella and the Graveyard Shift proposent des compositions originales folk pleines de mélodies et d’émotions à travers laquelle ils dépeignent des histoires, des rencontres, des souvenirs. Une guitare, un banjo, une contre basse et trois voix au service d’une musique inspirée par la folk bluegrass/indie et le rock des années 60 qui se veut pleine de bonnes vibrations.</p>
       </div>
@@ -25,7 +25,7 @@
           <h3 class="container__h3">Dudes Of Groove Society<br>(New Funk)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/DOGS.png" alt="Groupe Dudes of Groove Society">
-              <img class="container__picture__youtube" src="../assets/social/facebook.png" onclick="location.href='https://www.youtube.com/watch?v=I_xf_2gryrE';" />
+              <img class="container__picture__youtube" src="../assets/social/youtube.png" onclick="location.href='https://www.youtube.com/watch?v=I_xf_2gryrE';" />
           </picture>
           <p class="container__p">Le groupe préfère définir son style en “New Funk – Hip-Hop” et ne s’embête pas pour mélanger l’éléctro et les musiques actuelles avec la funk de James Brown, ce qu’il en ressort c’est une énergie folle et un show carré au possible !</p>
       </div>
@@ -33,7 +33,7 @@
           <h3 class="container__h3">Tadeo<br>(GROOVE / Hip Hop)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/Tadeos.jpeg" alt="Groupe Tadeo">
-              <img class="container__picture__youtube" src="../assets/social/facebook.png" onclick="location.href='https://www.youtube.com/watch?v=KN_XsZ9GDjQ';" />
+              <img class="container__picture__youtube" src="../assets/social/youtube.png" onclick="location.href='https://www.youtube.com/watch?v=KN_XsZ9GDjQ';" />
           </picture>
           <p class="container__p">Une envolée de mots posée sur un groove de base Hip Hop. Les textes sentent le bon vin et s'appuient sur des sonorités swing ou funk permettant le déhanchement et la danse !</p>
       </div>
@@ -42,7 +42,7 @@
           <h3 class="container__h3">Crucial Rob - Sound System<br>(REGGAE / DUB)</h3>
           <picture class="container__picture">
               <img class="container__picture__img" src="../assets/crucial_rob.jpg" alt="Groupe Crucial Rob">
-              <img class="container__picture__youtube" src="../assets/social/facebook.png" onclick="location.href='https://www.youtube.com/watch?v=hUpRt-x4oe8';" />
+              <img class="container__picture__youtube" src="../assets/social/youtube.png" onclick="location.href='https://www.youtube.com/watch?v=hUpRt-x4oe8';" />
           </picture>
           <p class="container__p">Nourrit d’univers musicaux et de cultures du monde entier, il est envieux de pouvoir concocter ses propres sonorités, il créé alors le Prophecy Studio. <br>
 
